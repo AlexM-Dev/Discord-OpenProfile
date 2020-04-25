b9392790e6ce95e6b6b5b52e217248c19163d24a
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2020-04-25 21:13:10 +1000

    Add endpoints class.

---
+++ Discord OpenProfile/Collections/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_OpenProfile.Collections {
+    /* 
+     * Stores all the Discord API endpoints required for Data Collection.
+     */
+    static class Endpoints {
+        public const string AddFriend = "";
+        public const string GetFriend = "";
+        public const string RemoveFriend = "";
+        public const string EnumerateFriends = "";
+    }
+}
