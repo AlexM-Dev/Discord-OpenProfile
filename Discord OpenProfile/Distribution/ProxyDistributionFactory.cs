b325e630c0d7db0f269cfb8821c36bfd9469cb02
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2020-04-25 23:52:05 +1000

    Identifying ideas for proxy and token distribution.

---
+++ Discord OpenProfile/Distribution/ProxyDistributionFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_OpenProfile.Distribution {
+    /*
+     * Assigns a set of proxies to a set of tokens (and vice versa).
+     * Operations done with a set of tokens are always used with the proxy
+     * given. 
+     * 
+     * Proxies and tokens chosen are done in batches. For example, a single token
+     * must utilise the UnknownUser Collections class alone while another might
+     * gain data on it.
+     * 
+     * The factory will also define the cycle for which proxies and
+     * tokens are used. I.e. after completing the identification process,
+     * the cycle will increment and another token will be used, and so on.
+     * This is to mitigate suspicion from Discord. There are also rate limits
+     * which should *probably* be monitored.
+     * 
+     * This is relying on the hope that proxies have no limit to the number of 
+     * requests or data transfer.
+     */
+    class ProxyDistributionFactory {
+    }
+}
