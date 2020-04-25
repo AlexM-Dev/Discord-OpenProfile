72f2dbf0d8f96c62193912cf5f76a3b38dd321e3
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2020-04-25 23:41:40 +1000

    Begin implementing the Unknown User identification process.

---
+++ Discord OpenProfile/Collections/UnknownUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord_OpenProfile.Storage;
+
+namespace Discord_OpenProfile.Collections {
+    /*
+     * UnknownUser class is designed to identify a user via their Name
+     * and Discriminator. This is done by adding the user as a friend, 
+     * enumerating all friends (which includes pending friend requests)
+     * and then grabbing that user. If desired, the request can be removed,
+     * making it *nearly* invisible.
+     * 
+     * This is a flaw by design of Discord and therefore isn't particularly
+     * patchable. It is the only way to get access to a user's profile without
+     * knowing their ID. 
+     */
+    class UnknownUser {
+        public async Task<UserReference> IdentifyUser(UserReference userRef, bool remove) {
+            // Add the friend here.
+            if (!await addUserFriend(userRef)) {
+                // Couldn't add the user for some reason.
+            }
+
+            // Enumerate the friends.
+            var enumUser = await grabUserFromFriends(userRef);
+            if (enumUser == userRef) {
+                // Couldn't enumerate the user for some reason.
+            }
+
+            // Remove the friend if applicable.
+            if (remove) {
+                if (!await removeUserFriend(userRef)) {
+                    // Couldn't remove the user.
+                }
+            }
+
+            return enumUser;
+        }
+
+        private async Task<bool> addUserFriend(UserReference userRef) {
+            return true;
+        }
+
+        private async Task<UserReference> grabUserFromFriends(UserReference userRef) {
+            return new UserReference(-1);
+        }
+
+        private async Task<bool> removeUserFriend(UserReference userRef) {
+            return true;
+        }
+    }
+}
---
+++ Discord OpenProfile/Storage/UserReference.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_OpenProfile.Storage {
+    /*
+     * Stores a reference to a user via their ID or Name & Discriminator.
+     * The latter is only temporary as the program aims to collect the ID.
+     * The Name & Discriminator are only retrievable assuming
+     * (a) the user hasn't blocked you,
+     * (b) the user has their friend requests on.
+     */
+    struct UserReference {
+        public long Id { get; set; } 
+        public string Name { get; set; }
+        public string Discriminator { get; set; }
+
+        public UserReference(string name, string discrim) {
+            if (!Int32.TryParse(discrim, out int discrimInt)) {
+                throw new Exception("Invalid user.");
+            }
+
+            this.Name = name;
+            this.Discriminator = discrim;
+            this.Id = -1;
+        }
+
+        public UserReference(long id) {
+            this.Id = id;
+            this.Name = null;
+            this.Discriminator = null;
+        }
+
+        public bool IsUnknown() {
+            return (Id == -1 || String.IsNullOrEmpty(Name) || 
+                String.IsNullOrEmpty(Discriminator) ||
+                !Int32.TryParse(Discriminator, out int _));
+        }
+
+        public static bool operator ==(UserReference a, UserReference b) {
+            return a.Id == b.Id && 
+                String.Equals(a.Name, b.Name) && 
+                String.Equals(a.Discriminator, b.Discriminator);
+        }
+
+        public static bool operator !=(UserReference a, UserReference b) {
+            return !(a == b);
+        }
+    }
+}
